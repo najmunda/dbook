c649578c61f17ce0202979033eebecd16e4065ec
Najmunda <najmundaakmal@gmail.com>
2024-03-21 13:14:47 +0700

    Temp Commit

--- js/index.js
+++ js/index.js
@@ -8,6 +8,19 @@
 }
 */
 
+// Check browser compatibility on local storage
 function isLocalStorageExist() {
   return typeof(Storage) !== 'undefined' ? true : false;
 }
+
+let addForm = document.getElementById("add-form")
+
+// 
+addForm.addEventListener('submit', function(event) {
+  let name = document.getElementById('name').value;
+  let author = document.getElementById('author').value;
+  let year = document.getElementById('year').value;
+  let isComplete = document.getElementById('is-complete').value;
+  console.log(name, author, year, isComplete);
+  event.preventDefault();
+});
